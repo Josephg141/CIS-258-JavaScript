51eab6010c5b72a13783b11aaf04cb1f7d72986b
Josephg17 <gjelajjoseph313@gmail.com>
2024-05-30 03:18:31 -0400

    project 4 done

--- joseph projects/project04/project01-04_txt.js
+++ joseph projects/project04/project01-04_txt.js
@@ -2,11 +2,30 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Joseph Gjelaj
+    Date:   5/30/24
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
-
+var homeStreet = "1 Main St.",
+    homeCity = "Sicilia",
+    homeState = "MA",
+    homeCode = "02103",
+    workStreet = "15 Oak Ln.",
+    workCity = "Central City",
+    workState = "MA",
+    workCode = "02104";
+    function setHomeAddress() {
+        document.getElementById("street").value = homeStreet;
+        document.getElementById("city").value = homeCity;
+        document.getElementById("state").value = homeState;
+        document.getElementById("code").value = homeCode;
+    }
+    function setWorkAddress() {
+        document.getElementById("street").value = workStreet;
+        document.getElementById("city").value = workCity;
+        document.getElementById("state").value = workState;
+        document.getElementById("code").value = workCode;
+    }
